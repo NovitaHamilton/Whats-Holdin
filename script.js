dbcd7c1e002254fee65a823e750acf1b95f12d50
Nicole Rue <nicolerue92@gmail.com>
2023-09-07 14:17:41 -0700

    add new function

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-'use strict';
+"use strict";
 let score = 20;
 let highscore = 0;
 const maxAttempts = 3;
 let attempts = 0;
 
 const displayMessage = function (message) {
-  document.querySelector('.message').textContent = message;
+  document.querySelector(".message").textContent = message;
 };
 
 const displayScore = function (score) {
-  document.querySelector('.score').textContent = score;
+  document.querySelector(".score").textContent = score;
 };
 
-const words = ['jug', 'crimp', 'sloper', 'pocket', 'pinch'];
+const words = ["jug", "crimp", "sloper", "pocket", "pinch"];
 const hints = [
-  'Makes you feel like you can conquer the world',
-  'Feels like tiny razor blades for your fingers',
-  'Might look innocent, but a sly fox trying to trick you into falling off the wall',
-  'Might as well pull a pulley',
-  'Squeeze em tight and feel the burn',
+  "Makes you feel like you can conquer the world",
+  "Feels like tiny razor blades for your fingers",
+  "Might look innocent, but a sly fox trying to trick you into falling off the wall",
+  "Might as well pull a pulley",
+  "Squeeze em tight and feel the burn",
 ];
 let randomIndex = Math.trunc(Math.random() * words.length);
 
@@ -36,10 +36,10 @@ function getHint() {
 }
 let hint = getHint();
 console.log(hint);
-document.querySelector('.hint').textContent = hint;
+document.querySelector(".hint").textContent = hint;
 
-document.querySelector('.check').addEventListener('click', function () {
-  const guess = document.querySelector('.guess').value;
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = document.querySelector(".guess").value;
   // When there's no input
   if (!guess) {
     displayMessage(
@@ -47,13 +47,13 @@ document.querySelector('.check').addEventListener('click', function () {
     );
   } else if (guess === secretWord) {
     displayMessage("That's right! 🎉");
-    document.querySelector('.number').textContent = secretWord;
-    document.querySelector('body').style.backgroundColor = '#648eb2';
-    document.querySelector('.number').style.width = '60rem';
-    document.querySelector('.message').style.color = '#eee';
+    document.querySelector(".number").textContent = secretWord;
+    document.querySelector("body").style.backgroundColor = "#648eb2";
+    document.querySelector(".number").style.width = "60rem";
+    document.querySelector(".message").style.color = "#eee";
     if (score > highscore) {
       highscore = score;
-      document.querySelector('.highscore').textContent = highscore;
+      document.querySelector(".highscore").textContent = highscore;
     }
   } else if (guess !== secretWord) {
     attempts++;
@@ -63,7 +63,7 @@ document.querySelector('.check').addEventListener('click', function () {
           maxAttempts - attempts
         } attempts left.`
       );
-      document.querySelector('.message').style.color = '#f80404';
+      document.querySelector(".message").style.color = "#f80404";
       score--;
       displayScore(score);
     } else {
@@ -74,18 +74,26 @@ document.querySelector('.check').addEventListener('click', function () {
     }
   }
 });
-document.querySelector('.again').addEventListener('click', function () {
+document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   randomIndex = Math.trunc(Math.random() * words.length);
   secretWord = getRandomWord();
   hint = getHint();
-  document.querySelector('.hint').textContent = hint;
-  document.querySelector('.number').textContent = '?';
-  displayMessage('Start guessing...');
-  document.querySelector('.message').style.color = '#eee';
-  document.querySelector('.guess').value = '';
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.number').style.width = '15rem';
+  document.querySelector(".hint").textContent = hint;
+  document.querySelector(".number").textContent = "?";
+  displayMessage("Start guessing...");
+  document.querySelector(".message").style.color = "#eee";
+  document.querySelector(".guess").value = "";
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
   displayScore(score);
   attempts = 0;
 });
+
+function climbing(typeOfClimbArray) {
+  if (typeOfClimbArray.includes("jug")) {
+    console.log("This climb should be climbed");
+  } else {
+    console.log("NAH");
+  }
+}
